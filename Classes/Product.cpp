c8833b684d54e510daae0e10ddaa0f571fe31ce4
corbino91 <corbinc@nmsu.edu>
2013-07-29 20:11:06 -0400

    Product class working version

--- Classes/Product.cpp
+++ Classes/Product.cpp
@@ -1,73 +1,31 @@
-#include <iostream>
-#include <string>
+#include "Product.h"
 
-using namespace std;
-
-class Product
-{
-public:
-	Product(string);
-	~Product();
-	void setQuantity(int);
-	void setPrice(double);
-	void setId(string);
-
-	int getQuantity();
-	double getPrice();
-	string getId();
-
-protected:
-	int quantity;
-	double price;
-	string id;
-};
-
-Product::Product(string name)
+// constructor
+Product::Product(string name, double cost, int num)
 {
 	id = name;
+	price = cost;
+	quantity = num;
 }
 
+// destructor
 Product::~Product()
 {
 
 }
 
 // setters
-void Product::setQuantity(int num)
+/*void Product::setQuantity(int num)
 {
 	quantity = num;
-}
+}*/
 
 void Product::setPrice(double p)
 {
 	price = p;
 }
 
-void Product::setId(string name)
+/*void Product::setId(string name)
 {
 	id = name;
-}
-
-// getters
-int Product::getQuantity()
-{
-	return quantity;
-}
-
-double Product::getPrice()
-{
-	return price;
-}
-
-string Product::getId()
-{
-	return id;
-}
-
-int main()
-{
-	Product ham("test");
-
-	//compute.setSpeed(25);
-	cout << ham.getId() << endl;
-}
+}*/
